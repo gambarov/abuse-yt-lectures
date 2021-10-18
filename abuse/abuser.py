ff11344b1e0d73a26790d576ac22d7f7ca40ee32
gambarov <gambarov.20015960@gmail.com>
2021-10-18 14:22:25 +0500

    refactoring

--- abuse/abuser.py
+++ abuse/abuser.py
@@ -6,7 +6,7 @@ import logging
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 
 
@@ -22,31 +22,33 @@ class LectureAbuser():
 
         self.driver.maximize_window()
 
-        login = config.get('Account', 'Login')
-        password = config.get('Account', 'Password')
-        channelName = config.get('Account', 'ChannelName')
+        print('Авторизация...')
 
-        if not self.service.auth(login, password, channelName):
+        if not self.service.auth(
+                config.get('Account', 'Login'),
+                config.get('Account', 'Password'),
+                config.get('Account', 'ChannelName')):
             print('Не удалось авторизоваться, попробуйте еще раз.')
             return
 
         time.sleep(1)
 
-        initComment = config.get('General', 'InitComment')
-        updComment = config.get('General', 'UpdComment')
-
         for videoUrl in videoUrls:
-            if not self.process_video(videoUrl, initComment, updComment):
+            if not self.process_video(videoUrl,
+                    config.get('General', 'InitComment'),
+                    config.get('General', 'UpdComment')):
                 print(f'Не удалось обработать видео {videoUrl}.')
                 time.sleep(1000)
+            else:
+                print(f'Успешно просмотрено видео {videoUrl}')
 
-    def process_video(self, videoUrl, initComment, updComment):
+    def process_video(self, videoUrl: str, initComment: str, updComment: str, delay: int = None):
         try:
             print(f'Открытие видео {videoUrl}...')
             self.driver.get(videoUrl)
 
             # Ждем загрузки страницы
-            WebDriverWait(self.driver, 15).until(EC.presence_of_element_located(
+            wait(self.driver, 15).until(EC.presence_of_element_located(
                 (By.XPATH, '//*[@id="container"]/h1')))
 
             # Пропуск возможной рекламы
@@ -59,14 +61,16 @@ class LectureAbuser():
             if self.service.insert_comment(initComment):
                 print(f'Написан начальный комментарий "{initComment}".')
 
-            duration = self.service.get_video_duration()
-            print(f'Ожидание {duration} секунд...')
-            time.sleep(duration / 2)
+            if delay is None:
+                delay = self.service.get_video_duration()
+
+            print(f'Ожидание {delay} секунд...')
+            time.sleep(delay / 2)
             # Останавливаем видео на половине
             # Если остановить в начале, то гугл может посчитать за спам
             self.driver.execute_script(
                 "document.getElementsByClassName('ytp-large-play-button')[0].click()")
-            time.sleep(duration / 2 + 5)
+            time.sleep(delay / 2 + 5)
 
             self.driver.execute_script("window.scrollBy(0,300)")
 
@@ -78,12 +82,12 @@ class LectureAbuser():
             # Отмечаем свой просмотр на сайте пердуна
             videoId = videoUrl.replace(
                 'https://www.youtube.com/watch?v=', '', 1)
-            self.driver.get(f"http://hsm.ugatu.su/yt/wtload.php?videoId={videoId}")
+            self.driver.get(
+                f"http://hsm.ugatu.su/yt/wtload.php?videoId={videoId}")
 
             time.sleep(1)
 
             logging.info(f'Success for {videoUrl}')
-            print(f'Успешно просмотрено видео {videoUrl}')
             return True
         except Exception as e:
             logging.exception(e)
--- abuse/service.py
+++ abuse/service.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait as wait
 
 
 class YTService:
@@ -17,26 +17,26 @@ class YTService:
         try:
             self.driver.get("https://accounts.google.com/signin")
 
-            loginBox = WebDriverWait(self.driver, 15).until(
+            loginBox = wait(self.driver, 15).until(
                 EC.presence_of_element_located((By.NAME, 'identifier')))
             loginBox.send_keys(login)
 
             self.driver.find_element(By.ID, 'identifierNext').click()
 
-            passwordBox = WebDriverWait(self.driver, 15).until(
+            passwordBox = wait(self.driver, 15).until(
                 EC.element_to_be_clickable((By.NAME, 'password')))
             passwordBox.send_keys(password)
 
             self.driver.find_element(By.ID, 'passwordNext').click()
 
             # Ждем перенаправления на стр. аккаунта (сразу или после того того, как пользователь пройдет двухфакторку)
-            WebDriverWait(self.driver, 120).until(
+            wait(self.driver, 120).until(
                 EC.url_contains('https://myaccount.google.com/'))
 
             self.driver.get('https://www.youtube.com/')
 
             # Выбираем нужный канал, с которого будем смотреть видео
-            accountBtn = WebDriverWait(self.driver, 15).until(
+            accountBtn = wait(self.driver, 15).until(
                 EC.element_to_be_clickable(
                     (By.XPATH, f"//*[contains(text(), '{channelName}')]"))
             )
@@ -47,44 +47,36 @@ class YTService:
         return False
 
     def get_video_duration(self):
-        duration = self.driver.find_elements_by_xpath(
+        text = self.driver.find_elements_by_xpath(
             "//span[@class='ytp-time-duration']")[0].text
-        x = time.strptime(duration, '%M:%S')
-        result = datetime.timedelta(
-            minutes=x.tm_min, seconds=x.tm_sec).total_seconds()
-        return int(result)
+        duration = time.strptime(text, '%M:%S')
+        seconds = datetime.timedelta(
+            minutes=duration.tm_min, seconds=duration.tm_sec).total_seconds()
+        return int(seconds)
 
     def insert_comment(self, initComment):
         # Кликаем на поле для написания коммента
-        commentBox = WebDriverWait(self.driver, 15).until(
+        commentBox = wait(self.driver, 15).until(
             EC.presence_of_element_located((By.ID, 'placeholder-area')))
         commentBox.click()
 
         # Печатаем комментарий
-        inputBox = WebDriverWait(self.driver, 15).until(
+        inputBox = wait(self.driver, 15).until(
             EC.presence_of_element_located((By.ID, 'contenteditable-root')))
         inputBox.send_keys(initComment)
 
         # Отправляем комментарий
-        submitBtn = WebDriverWait(self.driver, 15).until(
+        submitBtn = wait(self.driver, 15).until(
             EC.presence_of_element_located((By.ID, 'submit-button')))
         submitBtn.click()
         return True
 
     def update_comment(self, updComment):
         # Раскрываем меню действий (троеточие справа от комментария)
-        self.driver.execute_script(
-            """isClicked=false;
-                els=document.querySelectorAll('#button');
-                els.forEach((el)=>{
-                    if(el.ariaLabel=='Меню действий'&&isClicked==false){
-                        el.click();
-                        isClicked=true
-                    }
-                })""")
+        self.driver.execute_script("document.querySelector('button[aria-label=\"Меню действий\"]').click()")
 
         # Выбираем пункт с редактированием коммента
-        changeBtn = WebDriverWait(self.driver, 15).until(EC.element_to_be_clickable(
+        changeBtn = wait(self.driver, 15).until(EC.element_to_be_clickable(
             (By.XPATH, '//*[@id="items"]/ytd-menu-navigation-item-renderer[1]/a')))
         changeBtn.click()
 
@@ -101,7 +93,7 @@ class YTService:
     def skip_ad(self):
         # Пытаемся найти кнопку скипа рекламы (если есть, кнопка кликабельна через 5 сек.)
         try:
-            skipBtn = WebDriverWait(self.driver, 15).until(EC.element_to_be_clickable(
+            skipBtn = wait(self.driver, 15).until(EC.element_to_be_clickable(
                 (By.XPATH, '//*[@id="skip-button:6"]/span/button')))
             skipBtn.click()
             return True
